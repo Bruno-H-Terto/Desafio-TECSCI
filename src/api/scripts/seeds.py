3dc9948679fe1f0afd6362aa6107ab7c136cad87
Bruno Herculano <herculanoterto@gmail.com>
2025-05-14 21:18:24 -0300

    feat: improve script for generating seeds for plants, inverters, and metrics

--- src/api/scripts/seeds.py
+++ src/api/scripts/seeds.py
@@ -2,18 +2,36 @@ import json
 from datetime import datetime
 from pathlib import Path
 
-from api.models import Metric
-from config.database import get_session
+from api.models import Metric, Plant, Inverter, table_registry
+from config.database import get_session, engine
+
+print('Droped database')
+table_registry.metadata.drop_all(bind=engine)
+print('Create new database')
+table_registry.metadata.create_all(bind=engine)
+
+
+with get_session() as session:
+    plant1 = Plant(plant_name="Usina 1")
+    plant2 = Plant(plant_name="Usina 2")
+    session.add_all([plant1, plant2])
+    session.commit()
+
+    inverters_plant1 = [Inverter(plant_id=plant1.id) for _ in range(4)]
+    inverters_plant2 = [Inverter(plant_id=plant2.id) for _ in range(4)]
+
+    session.add_all(inverters_plant1 + inverters_plant2)
+    session.commit()
+    print('Plants and Inverters registered successfully')
+
 
 json_path = Path('src/api/sample/metrics.json')
-
 with open(json_path, 'r', encoding='utf-8') as file:
     raw_data = json.load(file)
 
 clean_data = []
 for item in raw_data:
     datetime_value = datetime.fromisoformat(item['datetime']['$date'].replace('Z', '+00:00'))
-
     power_value = item.get('potencia_ativa_watt', 0) or 0
     temp_celsius_value = item.get('temperatura_celsius', 0) or 0
 
@@ -26,13 +44,7 @@ for item in raw_data:
 
 with get_session() as session:
     for record in clean_data:
-        metric = Metric(
-            datetime=record['datetime'],
-            inverter_id=record['inverter_id'],
-            power=record['power'],
-            temp_celsius=record['temp_celsius'],
-        )
-        session.add(metric)
+        session.add(Metric(**record))
     session.commit()
 
-print('File registered successfully')
+print('Metrics registered successfully')
